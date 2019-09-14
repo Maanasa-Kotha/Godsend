28009ac2fb6de4100674a6133ea7b9083fbcd1f7
Alisha Fong <afangg91@gmail.com>
2019-09-14 17:53:04 -0400

    save frames

--- ImageHandler/videoCapture.py
+++ ImageHandler/videoCapture.py
@@ -1,6 +1,7 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+
 # Default resolutions of the frame are obtained.The default resolutions are system dependent.
 # We convert the resolutions from float to integer.
 frame_width = int(cap.get(3))
@@ -10,7 +11,7 @@ count = 0
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    cv2.imwrite("frame%d.png" % count, frame)     # save frame as JPEG file
+    cv2.imwrite("../images/videoFrames/frame%d.png" % count, frame)     # save frame as JPEG file
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
