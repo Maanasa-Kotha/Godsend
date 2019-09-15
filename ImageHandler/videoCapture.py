adcab76e66fc9dd219aac996b73e00c5357fa4c2
Alisha Fong <afangg91@gmail.com>
2019-09-15 00:21:33 -0400

    frames to text to compare

--- ImageHandler/videoCapture.py
+++ ImageHandler/videoCapture.py
@@ -1,32 +1,34 @@
 import cv2
 import pytesseract
 
-
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 # Default resolutions of the frame are obtained.The default resolutions are system dependent.
 # We convert the resolutions from float to integer.
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
-cap.set(28, 125)
-
 count = 0
 frameNum = 0
+refDic = None
+tempText = None
 print('start recording')
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    if count % 10 == 0:
-        cv2.imwrite("../images/videoFrames/frame" + str(frameNum%5) + ".png", frame)     # save frame as JPEG file
-        frameNum += 1
+    if count % 2 == 0:
 
-        # Our operations on the frame come here
+        # Check if current img is different than reference image and set new reference image if it is
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        text = pytesseract.image_to_string(gray)
-        print(text)
+        tempText = pytesseract.image_to_string(gray)
+
+        different = compare(refDic, tempText):
+        if refText == None or different: #If there is a reference image and there is a difference
+            print("Next Page")
+            refDic = dicPic(tempText)  
+
         # Display the resulting frame
         cv2.imshow('frame',gray)
 
