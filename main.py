0ab36ef0816cdd6c2bc5e634996bf4156135ffd9
Maanasa Kotha <marvelous@dhcp-18-21-235-30.dyn.mit.edu>
2019-09-15 04:15:18 -0400

    yeet

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from ImageHandler.opencvim import image_to_text, dic_pic
 
 import cv2
 import pytesseract
+import threading 
 # from ImageHandler.comparorator import compare
 
 
@@ -33,8 +34,24 @@ while(True):
         # Our operations on the frame come here
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        tempText = pytesseract.image_to_string(gray)
+        #edged = cv2.Canny(gray, 20, 200)
+        contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #print(contours)
+        threshold_area = 10000
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+           # rect = cv2.minAreaRect(cnt)
+            (x,y,w,h) = cv2.boundingRect(cnt)
+            #min_x, max_x = min(x, min_x), max(x+w, max_x)
+            #min_y, max_y = min(y, min_y), max(y+h, max_y)
 
+            if area > threshold_area:
+                cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
+                #cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+
+
+
+        tempText = pytesseract.image_to_string(gray)
         # if refDic == None:
         #     refDic = dic_pic(tempText)  
         # else:
@@ -45,11 +62,14 @@ while(True):
         #     else:
         #         print("stable")
         # Display the resulting frame
-        cv2.imshow('frame',gray)
+        cv2.imshow('frame', frame)
+    
+
+
         try:
-            text_to_speech(tempText)
+           text_to_speech(tempText)
         except AssertionError:
-            text_to_speech("Text complete")
+           text_to_speech("Text complete")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
