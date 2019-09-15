5abc1042376af5caaadd383e168a3b6e70f07285
Maanasa Kotha <marvelous@dhcp-18-21-235-30.dyn.mit.edu>
2019-09-14 20:43:14 -0400

    integration of image to text to speech

--- ImageHandler/opencvim.py
+++ ImageHandler/opencvim.py
@@ -6,29 +6,31 @@ import argparse
 
 print("we made it")
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, 
-	help="path to input image to be OCR'd")
-ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-	help="type of preprocessing to be done")
-args = vars(ap.parse_args())
+def image_to_text(image_text, preprocess):
+	# ap = argparse.ArgumentParser()
+	# ap.add_argument("-i", "--image", required=True, 
+	# 	help="path to input image to be OCR'd")
+	# ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	# 	help="type of preprocessing to be done")
+	# args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	image = cv2.imread(image_text)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
-	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	if preprocess == "thresh":
+		gray = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+	filename = "{}.png".format(os.getpid())
+	cv2.imwrite(filename, gray)
 
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
-print(text)
+	text = pytesseract.image_to_string(Image.open(filename))
+	os.remove(filename)
+	print(text)
 
-# show the output images
-cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
-cv2.waitKey(0)
+	# show the output images
+	cv2.imshow("Image", image)
+	cv2.imshow("Output", gray)
+	
+	return text
 
