6f7e3251877d07bf2aa0a65acf7959c3de11dacc
Alisha Fong <afangg91@gmail.com>
2019-09-14 20:51:05 -0400

    frame control

--- ImageHandler/videoCapture.py
+++ ImageHandler/videoCapture.py
@@ -6,12 +6,17 @@ cap = cv2.VideoCapture(0)
 # We convert the resolutions from float to integer.
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
+
 count = 0
+frameNum = 0
+print('start recording')
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    cv2.imwrite("../images/videoFrames/frame%d.png" % count, frame)     # save frame as JPEG file
+    if count % 5 == 0:
+        cv2.imwrite("../images/videoFrames/frame%d.png" % frameNum, frame)     # save frame as JPEG file
+        frameNum += 1
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
