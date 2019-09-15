c921b07b6f128f47b434c6275ac29184777ba594
Maanasa Kotha <marvelous@dhcp-18-21-235-30.dyn.mit.edu>
2019-09-15 11:08:04 -0400

    works webcame 3

--- main.py
+++ main.py
@@ -12,74 +12,74 @@ import threading
 
 
 cap = cv2.VideoCapture(0)
+if cap != None:
+    # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+    # We convert the resolutions from float to integer.
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
 
-# Default resolutions of the frame are obtained.The default resolutions are system dependent.
-# We convert the resolutions from float to integer.
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
+    cap.set(28, 125)
 
-cap.set(28, 125)
+    count = 0
+    frameNum = 0
+    print('start recording')
 
-count = 0
-frameNum = 0
-print('start recording')
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if count % 1 == 0:
+            cv2.imwrite("../images/videoFrames/frame" + str(frameNum%5) + ".png", frame)     # save frame as JPEG file
+            frameNum += 1
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if count % 1 == 0:
-        cv2.imwrite("../images/videoFrames/frame" + str(frameNum%5) + ".png", frame)     # save frame as JPEG file
-        frameNum += 1
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+            #edged = cv2.Canny(gray, 20, 200)
+            # contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # #print(contours)
+            # threshold_area = 10000
+            # min_area = 100000
+            # for cnt in contours:
+            #     area = cv2.contourArea(cnt)
+            #    # rect = cv2.minAreaRect(cnt)
+            #     #(x,y,w,h) = cv2.boundingRect(cnt)
+            #     # frame = frame[y:y+100, x-100:x+100]
+            #     #min_x, max_x = min(x, min_x), max(x+w, max_x)
+            #     #min_y, max_y = min(y, min_y), max(y+h, max_y)
 
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        #edged = cv2.Canny(gray, 20, 200)
-        # contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # #print(contours)
-        # threshold_area = 10000
-        # min_area = 100000
-        # for cnt in contours:
-        #     area = cv2.contourArea(cnt)
-        #    # rect = cv2.minAreaRect(cnt)
-        #     #(x,y,w,h) = cv2.boundingRect(cnt)
-        #     # frame = frame[y:y+100, x-100:x+100]
-        #     #min_x, max_x = min(x, min_x), max(x+w, max_x)
-        #     #min_y, max_y = min(y, min_y), max(y+h, max_y)
-
-        #     if area > threshold_area:
-        #         if area < min_area:
-        #             cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
-        #         #cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-
-
-
-        tempText = pytesseract.image_to_string(gray)
-        # if refDic == None:
-        #     refDic = dic_pic(tempText)  
-        # else:
-        #     different = compare(refDic, tempText)
-        #     if different: #If there is a reference image and there is a difference
-        #         print("Next Page")
-        #         refDic = dic_pic(tempText)  
-        #     else:
-        #         print("stable")
-        # Display the resulting frame
-        #cv2.imshow('frame', frame)
-        cv2.imshow('frame2', gray)
-    
+            #     if area > threshold_area:
+            #         if area < min_area:
+            #             cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
+            #         #cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
 
 
-        #try:
-        #   text_to_speech(tempText)
-        #except AssertionError:
-        #   text_to_speech("Text complete")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    count += 1
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+            tempText = pytesseract.image_to_string(gray)
+            # if refDic == None:
+            #     refDic = dic_pic(tempText)  
+            # else:
+            #     different = compare(refDic, tempText)
+            #     if different: #If there is a reference image and there is a difference
+            #         print("Next Page")
+            #         refDic = dic_pic(tempText)  
+            #     else:
+            #         print("stable")
+            # Display the resulting frame
+            #cv2.imshow('frame', frame)
+            cv2.imshow('frame2', gray)
+        
+
+
+            try:
+              text_to_speech(tempText)
+            except AssertionError:
+              text_to_speech("Text complete")
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        count += 1
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 
